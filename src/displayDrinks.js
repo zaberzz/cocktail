9ef8c49627131ef3e94a2dba1e76046a9c5b01bc
Saba <saba.sadegh67@gmail.com>
2022-12-24 17:01:52 +0330

    add functionallity to loading gif

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -1,9 +1,12 @@
 import get from "./getElement.js";
+import { hideLoading } from "./toggleLoading.js";
 
 const displayDrinks = ({ drinks }) => {
   const section = get(".section-center");
   const title = get(".title");
+
   if (!drinks) {
+    hideLoading();
     title.textContent = `Sorry, no result matched`;
     section.innerHTML = null;
     return;
@@ -12,12 +15,14 @@ const displayDrinks = ({ drinks }) => {
     .map((drink) => {
       const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
       return `  <a href="drink.html">
-    <article class="cocktail" data-id="${id}"></article>
+    <article class="cocktail" data-id="${id}">
     <img src="${image}" alt="${name}" />
     <h3>${name}</h3>
+    </article>
   </a>`;
     })
     .join("");
+  hideLoading();
   title.textContent = "";
   section.innerHTML = newDrinks;
   return section;
